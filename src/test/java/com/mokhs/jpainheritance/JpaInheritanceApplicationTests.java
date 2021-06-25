ea937c093b62a50b1aeed7095d18741d0727d98c
mokhs00 <mokhs00@naver.com>
2021-06-25 23:11:38 +0900

    Initial commit

---
+++ src/test/java/com/mokhs/jpainheritance/JpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokhs.jpainheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
